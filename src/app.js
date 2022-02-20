49233ec1b15c093bf3f872be080284cbd6e649d6
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-19 21:22:31 -0800

    setting up randomness

--- src/app.js
+++ src/app.js
@@ -3,9 +3,18 @@ function setup() {
 }
 
 function draw() {
+  background('#00000011')
   circle(50, 50, 10)
 }
 
 function mouseDragged() {
-  circle(mouseX, mouseY, 20)
+  let n = random()
+  let size = 20 + n * 20
+  let c1 = color('#2727e6')
+  let c2 = color('#ffffff')
+  let finalColor = lerpColor(c1, c2, n)
+
+  noStroke()
+  fill(finalColor)
+  circle(mouseX, mouseY, size)
 }
