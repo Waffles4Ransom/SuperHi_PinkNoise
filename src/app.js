0c83165e36c526a76976e32992304caa4c482f69
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-19 21:38:00 -0800

    replacing randomness with noise and noise level

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
+let t 
+let nl
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
+  background('#000000')
+   t = 0 
+   nl = 0.03
 }
 
 function draw() {
-  background('#00000011')
-  circle(50, 50, 10)
+  // background('#00000011')
 }
 
 function mouseDragged() {
-  let n = random()
-  let size = 20 + n * 20
+  // let n = random()
+  let n = noise(t * nl)
+  let size = 5 + n * 20
   let c1 = color('#2727e6')
   let c2 = color('#ffffff')
   let finalColor = lerpColor(c1, c2, n)
@@ -17,4 +24,5 @@ function mouseDragged() {
   noStroke()
   fill(finalColor)
   circle(mouseX, mouseY, size)
+  t += 1
 }
