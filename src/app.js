e66939190b6478f342dbb309f720e9f276b0ebb3
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-22 21:56:38 -0800

    changes the hue and noise space by using mouse coord vs time

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
   background('#000000')
    t = 0 
-   nl = 0.03
+   nl = 0.02
 }
 
 function draw() {
@@ -14,12 +14,15 @@ function draw() {
 }
 
 function mouseDragged() {
-  // let n = random()
-  let n = noise(t * nl)
+  // let n = noise(t * nl)
+  let n = noise(mouseX * nl, mouseY * nl)
   let size = 5 + n * 20
-  let c1 = color('#2727e6')
-  let c2 = color('#ffffff')
-  let finalColor = lerpColor(c1, c2, n)
+  // let c1 = color('#2727e6')
+  // let c2 = color('#ffffff')
+  // let finalColor = lerpColor(c1, c2, n)
+
+  colorMode(HSL, 100)
+  let finalColor = color(n * 100, 100, 50)
 
   noStroke()
   fill(finalColor)
