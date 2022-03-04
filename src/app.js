92400dab430491e0f67af440a706c97759442ff4
Jenna Ransom <jenna.ransom@gmail.com>
2022-03-03 16:14:49 -0800

    switches color and adds in a smoother fade in/out

--- src/app.js
+++ src/app.js
@@ -8,23 +8,27 @@ let totalY
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background('#000000')
-   t = 0 
-   nl = 0.02
-   density = 5
-   totalX = 200
-   totalY = 200
-   colorMode(HSL, 100)
+  t = 0 
+  nl = 0.02
+  density = 5
+  totalX = 200
+  totalY = 200
+  let c1 = color('#efd7e6')
+  let c2 = color('#f4450f')
 
-   for (let x = 0; x < totalX; x++) {
-     for (let y = 0; y < totalY; y++) {
-       let n = noise(x * nl, y * nl)
-       let rn = n * density - floor(n * density)
-       let finalColor = color(rn * 100, 100, 50)
-
-       setup(x, y, finalColor)
-     } 
-   }
-   updatePixels()
+  for (let x = 0; x < totalX; x++) {
+    for (let y = 0; y < totalY; y++) {
+      let n = noise(x * nl, y * nl)
+      let rn = n * density - floor(n * density)
+      let d = map(rn, 0, 0.5, 0, 1)
+      if (rn > 0.5) {
+        d = map(rn, 0.5, 1, 1, 0)
+      }
+      let finalColor = lerpColor(c2, c1, d)
+      setup(x, y, finalColor)
+    } 
+  }
+  updatePixels()
 }
 
 // function draw() {
